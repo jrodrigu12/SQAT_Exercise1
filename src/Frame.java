4817b9710f76752e5a5809f2026b2736e9efbbee
somename <someemail>
2016-09-22 15:04:00 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -1,12 +1,12 @@
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
-	
-	public Frame(int firstThrow, int secondThrow){
+
+	public Frame(int firstThrow, int secondThrow) {
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
 	}
-	
+
 	public int getFirstThrow() {
 		return firstThrow;
 	}
@@ -15,29 +15,36 @@ public class Frame {
 		return secondThrow;
 	}
 
-	//returns the score of a single frame
-	public int score(){
-		//to be implemented
+	// returns the score of a single frame
+	public int score() {
+		// to be implemented
 		int score = firstThrow + secondThrow;
 		return score;
 	}
 
-	//returns whether the frame is a strike or not
-	public boolean isStrike(){
-		//to be implemented
-		if(firstThrow == 10){
-			
+	// returns whether the frame is a strike or not
+	public boolean isStrike() {
+		// to be implemented
+		if (firstThrow == 10) {
+
 			return true;
-			
-		}else{
-		return false;
+
+		} else {
+			return false;
 		}
-		
+
 	}
-	
-	//return whether a frame is a spare or not
-	public boolean isSpare(){
-		//to be implemented
-		return false;
+
+	// return whether a frame is a spare or not
+	public boolean isSpare() {
+		// to be implemented
+		if (firstThrow + secondThrow == 10) {
+
+			return true;
+
+		} else {
+			return false;
+		}
+
 	}
 }
