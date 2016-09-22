a9e45e34f93c86c4ddc8b54669eb89cf7b4fa0da
somename <someemail>
2016-09-22 15:45:08 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
-	private List<Frame> frames = new ArrayList<Frame>();
+	private List<Frame> frames = new ArrayList<Frame>(9);
 	private Frame bonus;
 	
 	public BowlingGame(){}
@@ -15,20 +15,24 @@ public class BowlingGame {
 	public void addFrame(Frame frame){
 		//to be implemented
 		frames.add(frame);
-		
 	}
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
 		//to be implemented
+		
 	}
 	
 	// Returns the game score
 	public int score(){
 		//to be implemented: should return game score 
-		frames.forEach();
+		for (int i = frames.size()-1; i >= 0;--i){
+			
+			
+		}
 		
 		
+			
 		return 0;
 	}
 }
--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,13 +8,16 @@ public class TestBowling {
 	public void test() {
 		Frame frameOne = new Frame(5, 2);
 		int score= frameOne.score();
-		assertEquals("", 7, score);
+//		assertEquals("", 7, score);
 		Frame frameTwo = new Frame(5, 2);
 		
 		BowlingGame gameOne = new BowlingGame();
 		gameOne.addFrame(frameOne);
 		gameOne.addFrame(frameTwo);
 		
+		
+		int scoreGame = gameOne.score();
+		assertEquals("", 14, scoreGame);
 		
 		 
 	}
